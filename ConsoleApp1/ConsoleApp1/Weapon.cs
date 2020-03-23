7d2e56bd46c9ca9e2642f86e84c1e08193788d61
Dude39 <kiril.piskunovich39@gmail.com>
2020-03-23 18:50:56 +0300

    two commit

---
+++ ConsoleApp1/ConsoleApp1/Weapon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Weapon
+    {
+
+        private string name;
+        private float caliber;
+        private double distance;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public float Caliber
+        {
+            get
+            {
+                return caliber;
+            }
+            set
+            {
+                caliber = value;
+            }
+        }
+        public double Distance
+        {
+            get
+            {
+                return distance;
+            }
+            set
+            {
+                distance = value;
+            }
+        }
+
+    }
+}
